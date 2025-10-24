9c8c463233093b9cd79f78b0b50509830d41f2c1
umdelii <sang9205@gmail.com>
2025-10-24 12:09:06 +0900

    sql 파일 작성

---
+++ system.sql
@@ -0,0 +1,10 @@
+-- c##을 사용안하도록 설정
+-- 오라클버전이 업데이트되면서 사용자 아이디 앞에 c##을 붙이도록 설정되어 있음
+-- hr이름의 사용자 생성 => c##hr
+ALTER SESSION SET "_oracle_script"=TRUE;
+
+@C:\Users\soldesk\Downloads\db-sample-schemas-main\db-sample-schemas-main\human_resources\hr_install.SQL
+
+@C:\app\soldesk\product\21c\dbhomeXE\rdbms\admin\scott.sql
+
+sys AS sysdba
